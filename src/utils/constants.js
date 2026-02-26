5e8b050a176dcbfb2b7c8d00fe195b2d9ccad8c2
chx <chxhaixing@gmail.com>
2026-02-26 13:41:13 +0800

    refactor: 从 package.json 动态读取应用版本号

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 // 常量和默认配置
 
-// 应用版本号（从 package.json 同步）
-export const APP_VERSION = '0.1.1';
+// 应用版本号（从 package.json 读取）
+import packageJson from '../../package.json';
+export const APP_VERSION = packageJson.version;
 
 // 默认站点数据
 export const DEFAULT_SITES = [
